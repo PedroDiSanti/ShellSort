214931fbcda9d3f2031553441f923c75e82a86e5
Pedro Di Santi <pedro.disanti@truckpad.com.br>
2020-09-13 12:56:06 -0300

    Applying Knuth's increments.

--- main.c
+++ main.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
 
-void ShellSort(int *arr, int);
+void ShellSort(int *array, int);
 
 int main(){
-    int k, num;
+    int i, tamanhoArray;
     int array[8] = {9, 8, 3, 7, 5, 6, 4, 1};
 
-    num = sizeof(array)/sizeof(array[0]);
+    tamanhoArray = sizeof(array)/sizeof(array[0]);
 
     printf("\n Array now is: ");
-    for (k = 0; k < num; k++)
-        printf("%d ", array[k]);
+    for (i = 0; i < tamanhoArray; i++)
+        printf("%d ", array[i]);
 
-    ShellSort(array, num);
+    ShellSort(array, tamanhoArray);
 }
 
+void ShellSort(int *array, int tamanhoArray) {
+    int i , j , h = 1, valor;
 
+    while(h < tamanhoArray) {
+        h = 3 * h + 1;
+    }
 
-void ShellSort(int *arr, int num) {
-    int i, j, k, tmp;
-
-    for (i = num / 2; i > 0; i = i / 2){
-        for (j = i; j < num; j++){
-            for(k = j - i; k >= 0; k = k - i){
-                if (arr[k+i] >= arr[k])
-                    break;
-                else{
-                    tmp = arr[k];
-                    arr[k] = arr[k+i];
-                    arr[k+i] = tmp;
-                    printf("\n Array now is: ");
-                    for (k = 0; k < num; k++)
-                        printf("%d ", arr[k]);
-                }
+    while (h > 0) {
+        for(i = h; i < tamanhoArray; i++) {
+            valor = array[i];
+            j = i;
+            while (j > h-1 && valor <= array[j - h]) {
+                array[j] = array[j - h];
+                j = j - h;
             }
+            array[j] = valor;
         }
+        printf("\n Array now is: ");
+        for (i = 0; i < tamanhoArray; i++)
+            printf("%d ", array[i]);
+        h = h / 3;
     }
 }
